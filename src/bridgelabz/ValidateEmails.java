fe0a0394e0010dd48494fea1f6f8124c5f9f03b9
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-01-27 00:22:24 -0800

    SampleEmailValidation

---
+++ src/bridgelabz/ValidateEmails.java
@@ -0,0 +1,21 @@
+package bridgelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ValidateEmails {
+    private static boolean validateEmails(String email){
+        String regex = "^[a-z]{2,}[0-9a-z]+(?=.*[_.-])(?=.*[a-z0-9])[@]+[a-z0-1]{1,}[.]{1}[a-z]{2,4}([.]{0,1}[a-z]{2}){0,1}$";
+        Pattern pattern = Pattern.compile(regex);
+        if (email == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validateEmails("abc@yahoo.com"));
+    }
+}
